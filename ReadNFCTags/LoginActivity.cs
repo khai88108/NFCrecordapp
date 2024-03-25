cd81f10b8c03f5d08cff3d1ff645a51ac019a2fe
Khai88 <71096562+khai88108@users.noreply.github.com>
2024-03-25 17:15:42 +0800

    Add files via upload

---
+++ ReadNFCTags/LoginActivity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace SMARTpark
+{
+    [Activity(Label = "SMARTpark", MainLauncher = true) ]
+    public class LoginActivity : Activity
+    {
+        EditText email;
+        EditText password;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.login);
+
+
+            //Get email & password values from edit text  
+            email = FindViewById<EditText>(Resource.Id.userName);
+            password = FindViewById<EditText>(Resource.Id.password);
+            //Trigger click event of Login Button  
+            var logbutton = FindViewById<Button>(Resource.Id.login);
+            logbutton.Click += DoLogin;
+            var signupbutton = FindViewById<Button>(Resource.Id.Register);
+            signupbutton.Click += Btncreate_Click;
+        }
+        public void DoLogin(object sender, EventArgs e)
+        {
+            try
+            {
+                string dpPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "user.db3"); //Call Database  
+                var db = new SQLiteConnection(dpPath);
+                var data = db.Table<LoginTable>(); //Call Table  
+                var data1 = data.Where(x => x.username == email.Text && x.password == password.Text).FirstOrDefault(); //Linq Query 
+                if
+                (data1 != null)
+                {
+                Toast.MakeText(this, "Login successfully done!", ToastLength.Long).Show();
+                StartActivity(typeof(MainActivity));
+            }
+            else
+            {
+                //Toast.makeText(getActivity(), "Wrong credentials found!", Toast.LENGTH_LONG).show();  
+                Toast.MakeText(this, "Username or Password invalid!", ToastLength.Long).Show();
+            }
+        }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.ToString(), ToastLength.Short).Show();
+            }
+        }
+
+        private void Btncreate_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(signupActivity));
+        }
+        public string CreateDB()
+        {
+            var output = "";
+            output += "Creating Databse if it doesnt exists";
+            string dpPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "user.db3"); //Create New Database  
+            var db = new SQLiteConnection(dpPath);
+            output += "\n Database Created....";
+            return output;
+        }
+    }
+}
+
---
+++ ReadNFCTags/MainActivity.cs
@@ -0,0 +1,98 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Nfc;
+using Android.Content;
+using Android.Text;
+using System.Text;
+using System;
+
+namespace SMARTpark
+{
+    [Activity(Label = "SMARTpark", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        private TextView mTextView;
+        private NfcAdapter _nfcAdapter;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            _nfcAdapter = NfcAdapter.GetDefaultAdapter(this);
+            mTextView = FindViewById<TextView>(Resource.Id.textView1);
+           
+
+        }
+        protected override void OnResume()
+        {
+            try
+            {
+                base.OnResume();
+
+                if (_nfcAdapter == null)
+                {
+                    var alert = new Android.App.AlertDialog.Builder(this).Create();
+                    alert.SetMessage("NFC is not supported on this device.");
+                    alert.SetTitle("NFC Unavailable");
+                    alert.Show();
+                }
+                else
+                {
+                    // var tagDetected = new IntentFilter(NfcAdapter.ActionNdefDiscovered);
+                    var tagDetected = new IntentFilter(NfcAdapter.ActionTagDiscovered);
+                    var filters = new[] { tagDetected };
+
+                    var intent = new Intent(this, this.GetType()).AddFlags(ActivityFlags.SingleTop);
+
+                    var pendingIntent = PendingIntent.GetActivity(this, 0, intent, 0);
+
+                    _nfcAdapter.EnableForegroundDispatch(this, pendingIntent, filters, null);
+                }
+            }
+            catch (Exception ex) { Toast.MakeText(this, ex.Message, ToastLength.Long).Show(); }
+        }
+
+        protected override void OnNewIntent(Intent intent)
+
+        {
+
+
+            if (intent.Action == NfcAdapter.ActionTagDiscovered)
+            {
+                var tag = intent.GetParcelableExtra(NfcAdapter.ExtraTag) as Tag;
+                if (tag != null)
+                {
+                    // First get all the NdefMessage
+                    var rawMessages = intent.GetParcelableArrayExtra(NfcAdapter.ExtraNdefMessages);
+                    if (rawMessages != null)
+                    {
+                        var msg = (NdefMessage)rawMessages[0];
+
+                        // Get NdefRecord which contains the actual data
+                        var record = msg.GetRecords()[0];
+                        if (record != null)
+                        {
+                            if (record.Tnf == NdefRecord.TnfWellKnown) // The data is defined by the Record Type Definition (RTD) specification available from http://members.nfc-forum.org/specs/spec_list/
+                            {
+                                // Get the transfered data
+                                var data = Encoding.ASCII.GetString(record.GetPayload());
+                                mTextView.Text =  "Welcome to Sunway Pyramid Basement Parking.\n\nThanks For using our Smart Parking System.\n\n"+data;
+
+                                Toast.MakeText(this, "NFC tag detected! Record Success!  " , ToastLength.Long).Show();
+                                // }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+}
